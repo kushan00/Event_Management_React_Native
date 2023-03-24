923ce59e96297ea831399a2197064ec1a1fdb728
Kushan Dimantha <kushandimantha533@gmail.com>
2023-03-24 21:06:17 +0530

    chnages

--- Screen/Invitation_Management/InvitaionHome.js
+++ Screen/Invitation_Management/InvitaionHome.js
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
   invitationTitle2: {
     fontWeight: "bold",
     fontSize: 20,
-    color:"white"
+    color:"white" 
   },
   selectedInvitation: {
     backgroundColor: "#B27BDB",
